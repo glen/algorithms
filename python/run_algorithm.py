c23a245c9e54b454825a4b2b6667a1d1676236c4
Glen <glen.noronha@gmail.com>
2019-08-11 22:45:50 +0530

    Updated run_algorithm spec

--- python/run_algorithm.py
+++ python/run_algorithm.py
@@ -1,32 +1,54 @@
 import sys
 import pdb
 from termcolor import colored
-
-AVAILABLE_ALGORITHMS = ('linear_search')
-
-algorithm_to_run = sys.argv[1]
-
-if algorithm_to_run not in AVAILABLE_ALGORITHMS:
-  print( 'Available algorithms are ', AVAILABLE_ALGORITHMS )
-  sys.exit(0)
-
-package = 'python.algorithms'
 sys.path.append('../')
 from python.lib import read_data
 from python.lib import helper
-imported = getattr(__import__(package, fromlist=[algorithm_to_run]), algorithm_to_run)
 
-INPUT_FILE = 'data/input.json'
-target = int(sys.argv[2])
 
-input_data = read_data.read_json_file(INPUT_FILE)
-x = eval(f"imported.{helper.class_name(algorithm_to_run)}(input_data['input'])")
-located_position = x.search(target)
+class RunAlgorithm:
+    AVAILABLE_ALGORITHMS = ('linear_search')
+    INPUT_FILE = 'data/input.json'
 
-print(colored(' Linear Search Implementation '.center(60, '#'), 'grey', 'on_white'))
-print(helper.print_label('Input', 10, 'blue'), helper.print_value(input_data['input'], 'yellow'))
-print(helper.print_label('Target', 10, 'blue'), helper.print_value(target, 'yellow'))
-print(helper.print_label('Located', 10, 'blue'), helper.print_value('Yes', 'green') if located_position >=0 else helper.print_value('No', 'red') )
-if located_position >= 0:
-  print( helper.print_label('Position', 10, 'blue'), helper.print_value(located_position, 'yellow') )
+    def __init__(self):
+        self.parse_args(sys.argv)
+        self.load_algorithm(self.algorithm_to_run)
 
+    def parse_args(self, args=[]):
+        if len(args) < 3:
+            print('Invalid input!\nRun code like this "python run_algorithm [algorithm_name] [number_to_search]"')
+            sys.exit(0)
+
+        self.algorithm_to_run = sys.argv[1]
+
+        if self.algorithm_to_run not in self.AVAILABLE_ALGORITHMS:
+            print('Available algorithms are ', self.AVAILABLE_ALGORITHMS)
+            sys.exit(0)
+
+        self.target = int(sys.argv[2])
+
+    def load_algorithm(self, algorithm_to_load):
+        package = 'python.algorithms'
+        self.imported = getattr(__import__(package, fromlist=[algorithm_to_load]), algorithm_to_load)
+
+    def locate(self, target=None):
+        if target is None:
+            target = self.target
+        self.input_data = read_data.read_json_file(self.INPUT_FILE)
+        x = eval(f"self.imported.{helper.class_name(self.algorithm_to_run)}(self.input_data['input'])")
+        self.located_position = x.search(target)
+        return self.located_position
+
+    def print_result(self):
+        algorithm_name = helper.class_name(self.algorithm_to_run)
+        print(colored(f" {algorithm_name} ".center(60, '#'), 'grey', 'on_white'))
+        print(helper.print_label('Input', 10, 'blue'), helper.print_value(self.input_data['input'], 'yellow'))
+        print(helper.print_label('Target', 10, 'blue'), helper.print_value(self.target, 'yellow'))
+        print(helper.print_label('Located', 10, 'blue'), helper.print_value('Yes', 'green') if self.located_position >=0 else helper.print_value('No', 'red') )
+        if self.located_position >= 0:
+            print( helper.print_label('Position', 10, 'blue'), helper.print_value(self.located_position, 'yellow') )
+
+if __name__ == '__main__':
+    app = RunAlgorithm()
+    app.locate()
+    app.print_result()
---
+++ python/tests/test_run_algorithm.py
@@ -0,0 +1,48 @@
+import run_algorithm
+import unittest
+import sys
+import json
+from unittest.mock import patch
+from python.lib import read_data
+
+
+class runAlgorithmTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_no_command_line_arguments(self):
+        with patch.object(sys, 'argv', []):
+            with self.assertRaises(SystemExit):
+                run_algorithm.RunAlgorithm()
+
+    def test_single_command_line_argument(self):
+        with patch.object(sys, 'argv', ['python', 'linear_search']):
+            with self.assertRaises(SystemExit):
+                run_algorithm.RunAlgorithm()
+
+    def test_two_command_line_argument_but_incorrect_first_one(self):
+        with patch.object(run_algorithm.RunAlgorithm, 'AVAILABLE_ALGORITHMS', ('linear_search')):
+            with patch.object(sys, 'argv', ['python', 'linear_search1']):
+                with self.assertRaises(SystemExit):
+                    run_algorithm.RunAlgorithm()
+
+    def test_two_command_line_argument_with_target_present(self):
+        with patch.object(run_algorithm.RunAlgorithm, 'AVAILABLE_ALGORITHMS', ('linear_search')):
+            with patch.object(json, 'load', return_value={'input': [5, 7, 2, 3]}):
+                with patch.object(sys, 'argv', ['python', 'linear_search', 7]):
+                    self.assertEqual(run_algorithm.RunAlgorithm().locate(7), 1)
+
+    def test_two_command_line_argument_with_a_different_target_present(self):
+        with patch.object(run_algorithm.RunAlgorithm, 'AVAILABLE_ALGORITHMS', ('linear_search')):
+            with patch.object(json, 'load', return_value={'input': [5, 7, 2, 3]}):
+                with patch.object(sys, 'argv', ['python', 'linear_search', 3]):
+                    self.assertEqual(run_algorithm.RunAlgorithm().locate(3), 3)
+
+    def test_two_command_line_argument_with_target_absent(self):
+        with patch.object(run_algorithm.RunAlgorithm, 'AVAILABLE_ALGORITHMS', ('linear_search')):
+            with patch.object(json, 'load', return_value={'input': [5, 7, 2, 3]}):
+                with patch.object(sys, 'argv', ['python', 'linear_search', 12]):
+                    self.assertEqual(run_algorithm.RunAlgorithm().locate(12), -1)
+
+if __name__ == '__main__':
+    unittest.main()
