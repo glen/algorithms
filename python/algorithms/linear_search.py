6df0277b963f6675aaa0aa492b8938d1ecbe6aa4
Glen <glen.noronha@gmail.com>
2020-01-01 22:50:30 +0530

    Updated iterations in linear search algo

--- python/algorithms/linear_search.py
+++ python/algorithms/linear_search.py
@@ -1,11 +1,11 @@
 class LinearSearch:
     def __init__(self, search_array):
         self.search_array = search_array
-        self.iteration = -1
+        self.iteration = 0
 
     def search(self, target):
         for index, element in enumerate(self.search_array):
+            self.iteration = index + 1
             if target == element:
-                self.iteration = index
                 break
         return self.iteration
