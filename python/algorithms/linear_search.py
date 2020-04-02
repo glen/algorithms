658288761f55fa3631f574dd3f717da449d228e1
Glen <glen.noronha@gmail.com>
2020-04-02 13:41:06 +0530

    Fixed failing specs

--- python/algorithms/linear_search.py
+++ python/algorithms/linear_search.py
@@ -2,10 +2,11 @@ class LinearSearch:
     def __init__(self, search_array):
         self.search_array = search_array
         self.iteration = 0
+        self.located = -1
 
     def search(self, target):
         for index, element in enumerate(self.search_array):
             self.iteration = index + 1
             if target == element:
-                break
-        return self.iteration
+                return self.iteration
+        return self.located
